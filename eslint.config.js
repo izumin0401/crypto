86c45e44bd083ce20186fb8ee9116c7733b636ef
izumin0401 <unitedbz0401@gmail.com>
2024-11-04 15:14:01 +0900

    ESLintとPrettierの適用

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+// @ts-check
+import eslint from "@eslint/js";
+import angular from "angular-eslint";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
+      eslintConfigPrettier,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          indent: 2,
+          singleQuote: false,
+          trailingComma: "es5",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
